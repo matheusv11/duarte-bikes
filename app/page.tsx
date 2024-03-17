d471317b40cf4a72318112bcdc8607963d6c0049
matheusv11 <mathsales360@gmail.com>
2024-03-17 12:47:38 -0300

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,68 @@
-import {Button, Box, Typography } from '@mui/material';
+'use client'
+import {Button, Grid, Box, Typography } from '@mui/material';
 import Link from 'next/link';
+import AppBar from '@/app/ui/navigation/appbar'
+import Image from 'next/image';
+import logo from '@/public/images/instagram.webp';
+import FlorPreta from '@/public/images/bora-bill.webp';
+import BoraBill from '@/public/images/flor-preta.webp';
+import Main from './ui/navigation/main';
+import { longText } from '@/app/lib/mock';
+const open = false;
+const handleDrawer = () => {};
 
 export default function Home() {
   return (
-    <Box display="flex" justifyContent="center" margin="auto" flexDirection="column">
-      <Typography variant="body1">
-        Teste (Será uma home com informativos e links)
-      </Typography>
-      <Link href="/login">
-        <Button variant="contained">Login</Button>
-      </Link>
-    </Box>
+
+    <>
+    {/* Remover depois appbar */}
+    <AppBar open={open} handleDrawer={handleDrawer}/>
+    
+    <Main open={true}>
+      <Box sx={{display:"flex", flexDirection: 'column', background: 'pink', gap: 2}}>
+        <Image
+          src={logo}
+          className="mr-2 rounded-full"
+          // width={500}
+          style={{margin: 'auto'}}
+          height={350}
+          alt={`profile picture`}
+        />
+
+        <Box sx={{margin: 'auto'}}>
+          {/* Talvez usa flex mesmo, mas assim funciona bem */}
+          <Link href="/login" style={{marginRight: 20}}>
+            <Button color="success" variant="contained">Acessar</Button>
+          </Link>
+
+          <Link href="/register">
+            <Button variant="contained">Registrar-se</Button>
+          </Link>
+        </Box>
+
+        <Typography variant="body1">
+          {longText}
+        </Typography>
+
+        <Grid container spacing={2}>
+          {Array.from({ length: 22 }).map((currentElement, i) => (
+            <Grid item md={2} xs={12} key={i}>
+              <Image
+              src={FlorPreta}
+              className="mr-2 rounded-full"
+              // width={500}
+              style={{width: '100%'}}
+              height={200}
+              alt={`profile picture`}
+              />
+            </Grid>
+          ))}
+      
+        </Grid>
+
+      </Box>
+    </Main>
+
+    </>
   );
 }
---
+++ app/ui/navigation/main.tsx
@@ -0,0 +1,69 @@
+import { styled } from '@mui/material/styles';
+import { IconButton, Toolbar, Typography } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { drawerWidth } from "@/app/constants/drawer";
+
+interface AppBarComponent {
+  children: React.ReactNode;
+  open: boolean;
+}
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3), // Adicionar pra pagina inteira talvez
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+})); // Melhorar e adicionar apenas como um style
+
+
+export default function MainComponent({children, open }: AppBarComponent) {
+
+  return (
+    <Main open={open}>
+      <DrawerHeader/>
+      {children}
+    </Main>
+  );
+}
