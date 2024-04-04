131668a03387fe5a445ad110572e7bdd3abf7c1c
matheusv11 <mathsales360@gmail.com>
2024-04-04 16:04:59 -0300

    Translate date

--- src/providers/date-picker-provider.tsx
+++ src/providers/date-picker-provider.tsx
@@ -2,11 +2,12 @@
 
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDateFns } from '@mui/x-date-pickers-pro/AdapterDateFnsV3';
+import {ptBR} from 'date-fns/locale'
 
 export default function PickerProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <LocalizationProvider dateAdapter={AdapterDateFns}>{children}</LocalizationProvider>;
+  return <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>{children}</LocalizationProvider>;
 }
